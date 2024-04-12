f49800aeaea5a554808f89ddf89131cf848617cb
Mukesh Loganathan <mukeshloganathan12@gmail.com>
2024-04-12 08:17:13 -0400

    front end is more robust

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const digitalHealthTools = [
+  { id: 1, name: 'Tool 1', description: 'Description 1' },
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  const query = req.query.search;
+  const filteredTools = query ?
+    digitalHealthTools.filter(tool => tool.name.toLowerCase().includes(query.toLowerCase())) : digitalHealthTools;
+  res.render('index', { tools: filteredTools, searchQuery: query || '' });
 });
 
 module.exports = router;
