e03cc878f4143db2d8c3a4899f7cf6230409f86c
Mukesh Loganathan <mukeshloganathan12@gmail.com>
2024-04-14 10:04:16 -0400

    made changes to the database

--- controller/digital_health_tool_controller.js
+++ controller/digital_health_tool_controller.js
@@ -3,7 +3,11 @@ const asyncHandler = require("express-async-handler");
 const router = express.Router();
 
 const digitalHealthTools = [
-    { id: 1, name: 'Tool 1', description: 'Description 1' },
+    {id: 1, name: 'Linus', description: 'Cognitive assessment based on digitized clock drawing test'},
+    {id: 2, name: 'Cognoa', description: 'Medical diagnostic device to assist in autism diagnosis using computer vision on patients'},
+    {id: 3, name: 'Blueberry', description: 'On demand virtual pediatric care with physicians for 15$ a month'},
+    {id: 4, name: 'Curai', description: 'Virtual PCP entirely run by AI to augment provider workflow'},
+    {id: 5, name: 'Origin Health', description: 'Diagnotic tool for real time detection of fetal anomolies using ultrasound scans'}
   ];
 
 exports.digital_health_main_page = asyncHandler(async (req, res, next) => {
